24aec7c5cd503e219f76bbb7676013016cda01b5
Alejandro Melo Meza <ale.melo.meza@icloud.com>
2021-09-28 19:41:47 -0300

    feat: more advanced

--- handler.js
+++ handler.js
@@ -1,11 +1,18 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  let message = "Hello World";
+
+  const name = event.queryStringParameters && event.queryStringParameters.name;
+  if (name !== null) {
+    message = `Hello ${name}`;
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello World',
+        message: message,
         input: event,
       },
       null,
